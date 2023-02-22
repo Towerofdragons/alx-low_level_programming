36014e8b791820cb5eadfedb5be7b4ef1bc6d43c
Mark <marknjoropge@gmail.com>
2023-02-22 13:32:33 -0800

    Add 0x02. C task  2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times
+ *
+ * Return : nothing
+ */
+
+void print_alphabet_x10(void)
+{
+	int letter = 97;
+
+	for (int i = 0; i < 10; i++)
+	{
+		while (letter <= 122)
+		{
+			_putchar(letter);
+			letter++;
+		}
+		letter = 97;
+		_putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,6 @@
 #ifndef main_h
 #define main_h
-
+void print_alphabet_x10(void);
 void print_alphabet(void);
 int _putchar(char c);
 
