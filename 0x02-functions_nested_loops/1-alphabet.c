44a5ef9a44ece0c0d1703d9f1e7001af50beda14
Mark <marknjoropge@gmail.com>
2023-02-22 13:03:50 -0800

    Add 0x02. C task  1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the alphabet lowrcase followed by a new line
+ *
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	int letter = 97;
+
+	while (letter <= 122)
+	{
+		_putchar(letter);
+	}
+
+	_putchar('\n');
+}
