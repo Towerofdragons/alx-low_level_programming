7cb21007ab2540c160c96d3d4ef8adab90f556f5
Mark <marknjoropge@gmail.com>
2023-02-24 14:04:34 -0800

    Add 0x04. C task  4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints 0 - 9 excluding 2 and 4
+ *
+ * Return: nothing
+ */
+
+void print_most_numbers(void)
+{
+	int i = 48;
+
+	while (i <= 57)
+	{
+		if (i != 50 && i != 52)
+		{
+			_putchar(i);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
