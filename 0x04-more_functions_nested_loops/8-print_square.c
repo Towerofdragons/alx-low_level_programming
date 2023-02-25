aba11dd4a502eb062e364460fc3e2795fff1c08a
Mark <marknjoropge@gmail.com>
2023-02-25 02:01:00 -0800

    Add 0x04. C task  8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * print_square - print square followed by newline
+ * @size: desired size of square
+ *
+ * Return: nothing
+ */
+
+void print_square(int size)
+{
+	if (size > 0)
+	{
+		int width, height;
+
+		width = height = 0;
+
+		while (height < size)
+		{
+			while (width < size)
+			{
+				_putchar('#');
+				width ++;
+			}
+
+			_putchar('\n');
+
+			width = 0;
+			height ++;
+
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
