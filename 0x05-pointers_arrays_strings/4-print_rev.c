a997f641ab69572a5d3acab397be648669206fe8
Mark <marknjoropge@gmail.com>
2023-02-28 10:18:07 -0800

    Add 0x05. C task  4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse
+ * @s: input string
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+	while (s[i])
+	{
+		i++;
+	}
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+}
