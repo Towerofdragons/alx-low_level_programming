589b61e990bc713349150dea933db77f17868cab
Mark <marknjoropge@gmail.com>
2023-03-02 11:17:32 -0800

    Add 0x06. C task  3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strcmp - compare 2 strings
+ * @s1: string 1
+ * @s2: string 2
+ *
+ * Return: integer
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+
+	while (s1[i] != '\0' || s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			break;
+		}
+		i++;
+
+	}
+
+	return (s1[i] - s2[i]);
+}
