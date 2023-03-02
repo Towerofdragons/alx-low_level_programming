0193a8cd4987ed003b10eed24eb412dd6a067b9b
Mark <marknjoropge@gmail.com>
2023-03-02 09:41:26 -0800

    Add 0x06. C task  2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,10 +12,16 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for(i = 0; i+1 < n || src[i] != '\0'; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[i] = src [i];
 	}
 
-	return (dest);
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	return(dest);
 }
