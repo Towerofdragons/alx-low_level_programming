abe512142a7536f003f88df1fe2a0b07a454db05
Mark <marknjoropge@gmail.com>
2023-03-02 08:39:13 -0800

    Add 0x06. C task  1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strncat - concatenate two strings to a limit
+ * @dest: string to receive concatenation
+ * @src: string to be concatenated
+ * @n: limit the bytes for concatenation from src
+ *
+ * Return: string dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+
+	while (1)
+	{
+		if (dest[i] == '\0')
+		{
+			break;
+		}
+		i++;
+	}
+
+	while (1)
+	{
+
+		if (src[j] == '\0' || j + 1 == n)
+		{
+			dest[i] = src[j];
+			break;
+		}
+
+		dest[i] = src[j];
+
+		j++;
+		i++;
+
+	}
+	return (dest);
+}
