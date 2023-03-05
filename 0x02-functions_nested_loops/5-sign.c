3f4db72f54614caf79ca9d1291129e6624424851
Mark <marknjoropge@gmail.com>
2023-03-05 12:19:26 -0800

    Add 0x02-functions_nested_loops task  5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,15 +14,18 @@ int print_sign(int n)
 
 	if (n == 0)
 	{
+		_putchar('0');
 		sign = 0;
 	}
 
 	if (n > 0)
 	{
+		_putchar('+');
 		sign = 1;
 	}
 	if (n < 0)
 	{
+		_putchar('-');
 		sign = -1;
 	}
 
