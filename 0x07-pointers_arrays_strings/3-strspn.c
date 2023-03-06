5114aa0acd4de2362a150f8d9745a03d89f997c7
Mark <marknjoropge@gmail.com>
2023-03-06 11:54:55 -0800

    Add task  3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * _strspn - gets length of prefix substring
+ * @s: test substring
+ * @accept: chars accepted
+ *
+ * Return: length of substring
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	int j;
+	int truth;
+	unsigned int consistent;
+
+	consistent = 0;
+	i = 0;
+	j = 0;
+	truth = 1;
+
+	while (s[i] != '\0')
+	{
+		while (accept[j] != '\0')
+		{
+			if (accept[j] == s[i])
+			{
+				truth++;
+			}
+			j++;
+		}
+
+		if (truth == 0)
+		{
+			break;
+		}
+
+		truth = 0;
+		consistent++;
+		j = 0;
+		i++;
+	}
+	return (consistent);
+}
