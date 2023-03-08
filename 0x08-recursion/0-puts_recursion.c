2afd5bd354d364eed84609b5dfb4ec8fadd1b4ac
Mark <marknjoropge@gmail.com>
2023-03-08 07:18:47 -0800

    Add 0x08. C task  0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,7 +2,7 @@
 
 /**
  * _puts_recursion - prints a string recursively
- * @s - string for printing
+ * @s : string for printing
  *
  * Return : nothing
  */
@@ -10,18 +10,18 @@
 void _puts_recursion(char *s)
 {
 	int i;
-	
+
 	i = 0;
 
-	recur_forward (s, i);
+	recur_forward(s, i);
 
 _putchar ('\n');
 }
 
 /**
  * recur_forward - recursive function that prints
- * @s - string printed
- * @i - index to traverse string
+ * @s: string printed
+ * @i: index to traverse string
  *
  * Return: nothing
  */
@@ -32,7 +32,7 @@ void recur_forward(char *s, int i)
 	{/*base case*/
 
 		_putchar(s[i]);
-		recur_forward (s, i + 1);
+		recur_forward(s, i + 1);
 
 	}
 
