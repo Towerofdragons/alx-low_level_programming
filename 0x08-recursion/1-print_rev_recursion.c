bf03a78e8d26d742bd7c92377117c37e53f97c4b
Mark <marknjoropge@gmail.com>
2023-03-08 10:23:42 -0800

    Add 0x08. C task  1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,13 +1,27 @@
 #include "main.h"
 
+/**
+ * _print_rev_recursion - prints string in reverse
+ * @s - string
+ *
+ * Return: nothing
+ */
+
 void _print_rev_recursion(char *s)
 {
 	int i;
 
 	i = 0;
 	recur_back(s, i);
-	_putchar('\n');
 }
+
+/**
+ * recur_back - recursive function for printing
+ * @s: string
+ * @i: index 
+ *
+ * Return: nothing
+ */
 
 void recur_back(char *s, int i)
 {
