808a1b7543530530266770b06f2831e29503b0de
Mark <marknjoropge@gmail.com>
2023-03-10 08:08:47 -0800

    Add 0x0A-argc_argv task 2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
 
+/**
+ * main - print every provided arguement
+ * @argc: number of arguements
+ * @argv: array of provided arguements
+ *
+ * Return: 0
+ */
+
 int main(int argc, char *argv[])
 {
 	int i;
-	
+
 	i = 0;
 
-	while(i < argc)
+	while (i < argc)
 	{
-		printf("%s\n",argv[i]);
+		printf("%s\n", argv[i]);
 		i++;
 	}
+
+	return (0);
 }
