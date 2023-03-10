83b86b8a724df7611e2bdde5184d346e878a88bb
Mark <marknjoropge@gmail.com>
2023-03-10 07:31:07 -0800

    Add task 0x0A-argc_argv 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/** main - print name of function
+ * @argc: arguement count
+ * @argv: array of provided arguements
+ *
+ * Return: 0 
+ */
 int main(__attribute__((unused)) int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
