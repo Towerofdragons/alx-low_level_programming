dd5f7a26914132479c48b2ee0db7b37a6f8cbb98
Mark <marknjoropge@gmail.com>
2023-03-10 07:42:52 -0800

    Add task 0x0A-argc_argv 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print number of arguements passed
+ * @argc: number of arguements passed
+ * @argv: array of arguements provided
+ *
+ * Return: 0
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%i\n", argc);
+	return (0);
+}
