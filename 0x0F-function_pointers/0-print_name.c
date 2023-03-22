d7e16aefb7162de12f39708f8e2630e7546b9ee4
Mark <marknjoropge@gmail.com>
2023-03-22 05:27:54 -0700

    Add 0x0F-function_pointers task 0 and folder header file

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - runs the passed function printing names
+ * @name: name passed
+ * @f: function pointer passed
+ *
+ * Return: nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
