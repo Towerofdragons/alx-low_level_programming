68afab1a51ff274a7479425649ce6c40347b5a0d
Mark <marknjoropge@gmail.com>
2023-04-04 07:39:21 -0700

    Add 0x13 task 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+void free_listint(listint_t *head)
+{
+	list_free(head);
+}
+
+listint_t  *list_free(listint_t *head)
+{
+	if (head->next != NULL)
+	{
+		free(list_free(head->next));
+	}
+	return(head);
+}
