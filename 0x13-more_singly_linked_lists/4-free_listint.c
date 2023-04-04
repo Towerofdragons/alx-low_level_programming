c89001a2798426edfb8a91a0b1f4e3188891cdb9
Mark <marknjoropge@gmail.com>
2023-04-04 14:17:50 -0700

    Add 0x13 task 4

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,11 +1,23 @@
 #include "lists.h"
 
+/**
+ * free_listint - free given linked list
+ * @head: pointer to first node in list
+ *
+ * Return: nothing
+ */
 void free_listint(listint_t *head)
 {
 	list_free(head);
 	free(head);
 }
 
+/**
+ * list_free - recursive function to free linked list
+ * @head: pointer to first node in list
+ *
+ * Return: pointer to a node
+ */
 listint_t  *list_free(listint_t *head)
 {
 	if (head->next != NULL)
