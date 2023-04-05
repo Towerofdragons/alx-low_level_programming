6fca4a595712a9d37ce6dfa5957cf3e3baab8764
Mark <marknjoropge@gmail.com>
2023-04-05 10:50:20 -0700

    Add 0x13 task 6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/** 
+ * pop_listint - remove first node from list
+ * @head: pointer to pointer variable holding head
+ *
+ * Return: value in first node; 0 if list is empty
+ */
+int pop_listint(listint_t **head)
+{
+	int value;
+	listint_t *temp;
+
+	if (*head == NULL)
+		return (0);
+
+        temp = (*head)->next;
+	value = (*head)->n;
+	free(*head);
+	*head = temp;
+	return (value);
+}
