c18addbefaaba8676086a74926a2577a97d74d39
Mark <marknjoropge@gmail.com>
2023-07-07 19:45:51 +0300

    Add task 2

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,16 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - find index of a provided value
+ * @key: value
+ * @size: size of the table array
+ *
+ * Return: hash index
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size __attribute__((unused)))
+{
+	unsigned long int index;
+
+	index = hash_djb2(key);
+	return (index);
+}
