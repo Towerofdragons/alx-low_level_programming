c52d417e17ea1e3fbacb11ea20c9c5df48945c87
Mark <marknjoropge@gmail.com>
2023-07-08 12:21:00 +0300

    Add task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,9 +14,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
 	hash_node_t *new_node;
-	hash_node_t *curr;
-	char *new_value;
-	char *new_key;
 
 	if (ht == NULL)
 	{
@@ -27,90 +24,66 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	}
 	
+#if DEBUG
+	printf("%lu, %s\n", ht -> size, value);
+#endif
 	index = key_index((const unsigned char *)key, ht -> size);
-	printf("%lu:: INDEXING AT\n", index);
-
-	new_node = malloc(sizeof(hash_node_t));
-	if (new_node == NULL)
-	{
-		return (0);
-	}
-
-	new_value =  malloc(sizeof(char) * (strlen(value) + 1));
-	new_key = malloc(sizeof(char) * (strlen(key) + 1));
-	
-	if (new_value == NULL)
-	{
-		free(new_node);
-		return (0);
-	}
-
-	if (new_key == NULL)
-	{
-		free(new_node);
-		free(new_value);
-		return (0);
-	}
-
-	strcpy(new_value, value);
-	strcpy(new_key, key);
-
-	new_node -> key = new_key;  
-	new_node -> value = new_value;
 
 	if (ht -> array[index] == NULL)
 	{
-		new_node -> next =  NULL;
+#if DEBUG
+		printf("Empty INDEX: Creating new node\n");
+#endif
+		/*create new node*/
+		new_node = create_node((char *)key, (char *)value);
 		ht -> array[index] = new_node;
+#if DEBUG
+		printf("Node created successfully\n");
+		/*Testing new node*/
+ 		printf("%s\n", ht -> array[index] -> value);
+#endif
 		return(1);
 	}
-	else
-	{
-		curr = ht -> array[index];
-		while (curr != NULL)
-		{
-			if (strcmp(curr -> key, key) == 0)
-			{
-				if (strcmp(curr -> value, value) == 0)
-				{
-					free(new_value);
-					free(new_key);
-					free(new_node);
-					return (1);
-				}
-				else
-				{
-					curr -> value = new_value;
-					return (1);
-				}
-			}
-			curr = curr -> next;
-		}
 
-		new_node -> next = ht -> array[index];
-		ht -> array[index] = new_node;
-	}
+	return(1);
 
-	printf("Value Set\n");
-
-	return (1);
 
 }
-/*
-hash_table cannot be NULL
-KEY cannot be empty
-value can be empty - duplicate entry
 
-Add an element to the hash table
-	USE KEY TO FIND PLACE TO INSERT
-		hash value, find the index
-		at index: if index has NULL
-			add node
-		else check if key present
-			if key present
-				if replace value if needed
-			else
-				create node with key
-				add node at beginnig of list
+hash_node_t *create_node(char *key, char *value)
+{
+	hash_node_t *new;
+	char *new_key;
+	char *new_value;
 
-*/
+	new = malloc(sizeof(hash_node_t));
+	
+	if (new == NULL)
+	{
+		return(NULL);
+	}
+
+	new_key = malloc(sizeof(char) * (strlen(key) + 1));
+	if (new_key == NULL)
+	{
+		free(new);
+		return(NULL);
+	}
+
+	new_value = malloc(sizeof(char) * (strlen(value) + 1));
+        if (new_value == NULL)
+        {
+                free(new);
+		free(new_key);
+                return(NULL);
+        }
+
+	strcpy(new_key, key);
+	strcpy(new_value, value);
+
+	new -> key = new_key;
+	new -> value = new_value;
+	new -> next = NULL;
+	return(new);
+
+}
